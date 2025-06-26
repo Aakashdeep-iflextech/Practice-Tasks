753481784bf6f9b162cb433e3b8a0b178a8a42dc
Aakashdeep <aakashmauryaa122@gmail.com>
2025-06-26 16:32:44 +0530

    Add files via upload

---
+++ Day1/task2.js
@@ -0,0 +1,17 @@
+// function demonstrateHoisting() {
+//   console.log("Value of city before declaration:", city);
+//   // undefined due to hoisting
+
+//   var city = "Delhi";
+//   console.log("Value of city after declaration:", city);
+// }
+
+// demonstrateHoisting();
+
+
+demonstrateHoisting();
+
+function demonstrateHoisting() {
+    var city = "Delhi"
+    console.log("City :", city)
+}
---
+++ Day1/task3.js
@@ -0,0 +1,16 @@
+function usingOprators() {
+  let x = 10;
+  let y = 5;
+  let a = "10";
+
+  console.log("Addition:", x + y);
+  console.log("Subtraction:", x - y);
+  console.log("Multiplication:", x * y);
+  console.log("Division:", x / y);
+  console.log("Is x greater than y ?", x > y);
+  console.log("Is x equal to y ?", x == y);
+  console.log("Comparison x with a :", x == a);
+  console.log("Comparison Type x with a :", x === a);
+}
+
+usingOprators();
